e80c187de628eab9a606479e129509511d9782cc
jodyneckles <jody@technative.tv>
2019-02-04 09:18:47 +0000

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,32 +1,77 @@
-import React, { Component } from 'react'
 
-class ControlledInput extends Component {
-    state = {
-      firstName: 'John',
-      lastName: 'Henry'
-    }
+// src/components/ControlledInput.js
+import React from 'react'
 
-    // Form with 'CONTROLLED STATE COMPONENT'
-    handleFirstNameChange = event => {
-      this.setState({
-        firstName: event.target.value
-      })
-    }
+class ControlledInput extends React.Component {
+  state = {
+    myValue: ''
+  }
 
-      handleLastNameChange = event => {
-        this.setState({
-          lastName: event.target.value
-        })
-      }
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
-      render () {
-        return (
-          <form>
-            <input type='text' id='firstName' onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-            <input type='text' id='lastName' onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-          </form>
-        )
-      }
+  handleSubmit = event => {
+    event.preventDefault()
+    this.sendFormDataSomewhere(this.state)
+  }
+
+  render () {
+    return (
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input
+          type='text'
+          value={this.state.myValue}
+          onChange={this.handleChange}
+          name='myValue'
+        />
+      </form>
+    )
+  }
 }
 
 export default ControlledInput
+
+// import React, { Component } from 'react'
+
+// class ControlledInput extends Component {
+//     state = {
+//       firstName: 'John',
+//       lastName: 'Henry'
+//     }
+
+//     // Form with 'CONTROLLED STATE COMPONENT'
+//     // handleFirstNameChange = event => {
+//     //   this.setState({
+//     //     firstName: event.target.value
+//     //   })
+//     // }
+
+//     //   handleLastNameChange = event => {
+//     //     this.setState({
+//     //       lastName: event.target.value
+//     //     })
+//     //   }
+
+//     // Refactor both functions into one handler
+//     handleChange = event => {
+//       this.setState({
+//         [event.target.name]: event.target.value
+//       })
+//     }
+
+//     render () {
+//       return (
+//         <form>
+//           {/* <input type='text' id='firstName' onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+//           <input type='text' id='lastName' onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} /> */}
+//           <input type='text' id='firstName' onChange={event => this.handleChange(event)} value={this.state.firstName} />
+//           <input type='text' id='lastName' onChange={event => this.handleChange(event)} value={this.state.lastName} />
+//         </form>
+//       )
+//     }
+// }
+
+// export default ControlledInput
